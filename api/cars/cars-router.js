9dd8356565a87de3eb24dd440adc649e3c077098
rhammer35 <rhammer35@icloud.com>
2021-05-11 15:58:57 -0400

    created endpoints for the CR of CRUD in cars-router.js

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,1 +1,39 @@
-// DO YOUR MAGIC
+const express = require('express');
+
+const Cars = require('./cars-model');
+
+const { checkCarPayload, checkVinNumberValid, checkCarId } = require('./cars-middleware');
+
+const router = express.Router()
+
+router.get('/', (req, res, next) => {
+  Cars.getAll()
+    .then(cars => {
+      res.status(200).json(cars);
+    })
+    .catch(next)
+});
+
+router.get('/:id', checkCarId, (req, res) => {
+  res.status(200).json(req.car);
+});
+
+router.post('/', checkCarPayload, checkVinNumberValid, (req, res, next) => 
+  {
+    Cars.create(req.body)
+      .then(car => {
+        res.status(201).json(car);
+      })
+      .catch(next);
+  }
+);
+
+router.use( (err, req, res ) => {
+  res.status(err.status || 500).json({
+    note: "Something went wrong in the cars router",
+    message: err.message,
+    stack: err.stack
+  });
+});
+
+module.exports = router;
