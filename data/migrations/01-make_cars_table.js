05ce11210e7004c3824d97f1a97198167a5c7bd2
rhammer35 <rhammer35@icloud.com>
2021-05-11 15:53:32 -0400

    edited column names in migration 01

--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,13 +1,13 @@
 exports.up = function(knex) {
   return knex.schema.createTable('cars', table => {
-    // primary key fruit_id integers, autoincrementing...
-    table.increments('vehicle_id');
-    table.text('vin', 255).unique().notNullable();
-    table.text('make', 128).notNullable();
-    table.text('model', 128).notNullable();
-    table.float('mileage').notNullable();
-    table.text('title', 128);
-    table.text('transmission', 128);
+    // primary key car_id is an integer, autoincrementing...
+    table.increments('car_id');
+    table.text('car_vin', 255).unique().notNullable();
+    table.text('car_make', 128).notNullable();
+    table.text('car_model', 128).notNullable();
+    table.float('car_mileage').notNullable();
+    table.text('car_title', 128);
+    table.text('car_transmission', 128);
   });  
 };
 
