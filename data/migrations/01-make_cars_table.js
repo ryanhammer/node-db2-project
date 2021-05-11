24c5328b6c3e7359819869e8b37897f0864df4ff
rhammer35 <rhammer35@icloud.com>
2021-05-11 14:51:22 -0400

    cloned in project, created local branch, installed dependencies, added instructions to 01-make-cars-table migration to create cars table

--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,1 +1,17 @@
-// DO YOUR MAGIC
+exports.up = function(knex) {
+  return knex.schema.createTable('cars', table => {
+    // primary key fruit_id integers, autoincrementing...
+    table.increments('vehicle_id');
+    table.text('vin', 255).unique().notNullable();
+    table.text('make', 128).notNullable();
+    table.text('model', 128).notNullable();
+    table.float('mileage').notNullable();
+    table.text('title', 128);
+    table.text('transmission', 128);
+  });  
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExists('cars');
+};
+
