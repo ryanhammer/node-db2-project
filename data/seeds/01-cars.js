cb8c8ce342b8aa0ca33530031c9b5101443b2cc9
rhammer35 <rhammer35@icloud.com>
2021-05-11 21:38:09 -0400

    created seed data

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,38 @@
-// STRETCH
+exports.seed = async function(knex) {
+  // Truncates table - wipes it, resets primary key to 1
+  await knex('cars').truncate();
+
+  // Inserts seed entries
+  await knex('cars').insert([
+    {
+      vin: '13738492051111111',
+      make: 'ford',
+      model: 'F150',
+      mileage: 88291,
+      title: 'clean',
+      transmission: 'automatic'
+    },
+    {
+      vin: '11115672130891111',
+      make: 'toyota',
+      model: 'camry',
+      mileage: 18876,
+      title: 'clean',
+      transmission: 'auto'
+    },
+    {
+      vin: '11551111116784111',
+      make: 'subaru',
+      model: 'impreza',
+      mileage: 61855,
+      transmission: 'manual'
+    },
+    {
+      vin: '21551164667841113',
+      make: 'kia',
+      model: 'telluride',
+      mileage: 11721,
+      title: 'salvage'
+    }
+  ]);
+};
